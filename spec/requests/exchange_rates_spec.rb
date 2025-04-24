cd030c0fa0fdb52158f17c6322b8a216a4ee85af
Diego Echeverri <diegoeche@gmail.com>
2025-04-24 16:20:39 +0200

    Another example

--- spec/requests/exchange_rates_spec.rb
+++ spec/requests/exchange_rates_spec.rb
@@ -1,6 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe 'ExchangeRates', type: :request do
+  def json_response
+    JSON.parse(response.body)
+  end
+
   describe 'GET /index' do
     it 'responds with 200 on the list endpoint' do
       get '/exchange_rates'
@@ -11,9 +15,8 @@ RSpec.describe 'ExchangeRates', type: :request do
     it 'returns the right amount of entries' do
       get '/exchange_rates'
 
-      json_response = JSON.parse(response.body)
-
       total_currencies = ExchangeRatesController::EXCHANGE_CURRENCIES.length
+
       expect(json_response.length).to eq(total_currencies ** 2 - total_currencies)
     end
   end
@@ -27,8 +30,6 @@ RSpec.describe 'ExchangeRates', type: :request do
 
     it 'returns the right amount of entries' do
       get '/exchange_rates/usd'
-
-      json_response = JSON.parse(response.body)
 
       total_currencies = ExchangeRatesController::EXCHANGE_CURRENCIES.length
       expect(json_response.length).to eq(total_currencies - 1)
